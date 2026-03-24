30c670d70f3e2ebc4f26dcd37a26c188c1ed44b8
Milky104 <maxtendalton@gmail.com>
2026-03-24 15:47:21 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -8,53 +8,56 @@ const io = new Server(server);
 
 app.use(express.static(__dirname));
 
-let rooms = {};
+let rooms = {};      // stores {roomName: [users]}
+let messages = {};   // stores {roomName: [messages]}
 
 io.on('connection', socket => {
 
   socket.on('join-room', ({ room, username }) => {
     socket.join(room);
 
-    if (!rooms[room]) rooms[room] = [];
+    // Init room arrays if needed
+    if(!rooms[room]) rooms[room] = [];
+    if(!messages[room]) messages[room] = [];
+
     rooms[room].push({ id: socket.id, username });
-
-    io.to(room).emit('users', rooms[room].map(u => u.username));
-
     socket.room = room;
     socket.username = username;
+
+    // Send user list
+    io.to(room).emit('users', rooms[room].map(u => u.username));
+
+    // Send message history to new user
+    socket.emit('chat-history', messages[room]);
   });
 
   socket.on('chat', msg => {
-    io.to(socket.room).emit('chat', {
-      user: socket.username,
-      text: msg
-    });
+    const messageData = { user: socket.username, text: msg };
+
+    // Save message
+    if(socket.room) messages[socket.room].push(messageData);
+
+    // Broadcast
+    io.to(socket.room).emit('chat', messageData);
   });
 
   socket.on('disconnect', () => {
-    if (!socket.room) return;
+    if(!socket.room) return;
 
     rooms[socket.room] = rooms[socket.room].filter(u => u.id !== socket.id);
 
     io.to(socket.room).emit('users', rooms[socket.room].map(u => u.username));
   });
 
-  // VOICE
+  // Voice stuff
   socket.on('join-voice', () => {
-    if (!socket.room) return;
-    socket.to(socket.room).emit('user-joined', socket.id);
+    if(socket.room) socket.to(socket.room).emit('user-joined', socket.id);
   });
-
-  socket.on('offer', data => {
-    io.to(data.to).emit('offer', { ...data, from: socket.id });
-  });
-
-  socket.on('answer', data => {
-    io.to(data.to).emit('answer', { ...data, from: socket.id });
-  });
-
-  socket.on('ice', data => {
-    io.to(data.to).emit('ice', { ...data, from: socket.id });
+  socket.on('offer', data => { io.to(data.to).emit('offer', {...data, from: socket.id}); });
+  socket.on('answer', data => { io.to(data.to).emit('answer', {...data, from: socket.id}); });
+  socket.on('ice', data => { io.to(data.to).emit('ice', {...data, from: socket.id}); });
+  socket.on('speaking', speaking => {
+    if(socket.room) socket.to(socket.room).emit('speaking', { user: socket.username, speaking });
   });
 
 });
